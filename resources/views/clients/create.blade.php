d3cc20698b05a863402a883702a94654c99ce84b
Pau <pau.antunez.clavero@gmail.com>
2020-02-13 12:57:23 +0100

    treball amb vistes i formularis

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"> Cliente </div>
+                <div class="card-header text-center">Nuevo Cliente </div>
                 <div class="card-body">
                 {{ Form::open(['route' => 'clients.store']) }}
 
--- resources/views/clients/partials/form.blade.php
+++ resources/views/clients/partials/form.blade.php
@@ -1,34 +1,87 @@
-<div class="form-group">
-    {{ Form::label('nombre','Nombre') }}
-    {{ Form::text ('nombre',null,['class' =>'form-control'])}}
-</div>
-<div class="form-group">
-    {{ Form::label('calle','Direccion') }}
-    {{ Form::text ('calle',null,['class' =>'form-control'])}}
-</div>
-<div class="form-group">
-    {{ Form::label('poblacion','Población') }}
-    {{ Form::text ('poblacion',null,['class' =>'form-control'])}}
-</div>
-<div class="form-group">
-    {{ Form::label('mail','Email') }}
-    {{ Form::text ('mail',null,['class' =>'form-control'])}}
-</div>
-<div class="form-group">
+<div class="form-group">   
+    {{ Form::label('empresa_id','Empresa') }}
+    {{Form::select('empresa_id',$empresas, null, ['placeholder'=>'Seleccionar una Empresa'])}}
     {{ Form::label('comercial_id','Comercial') }}
     {{Form::select('comercial_id',$comercials, null,['placeholder'=>'Seleccionar un Comercial'])}}
 </div>
-<div class="form-group">
-    {{ Form::label('grupo','Grupo') }}
-    {{ Form::text ('grupo',null,['class' =>'form-control'])}}
+<div class="form-row">
+    <div class="form-group col-md-5">
+    {{ Form::label('nombre','Nombre') }}
+    {{ Form::text ('nombre',null,['class' =>'form-control'])}}
+    </div>
+
+    <div class="form-group col-md-3">    
+        {{ Form::label('cif','Cif') }}
+        {{ Form::text ('cif',null,['class' =>'form-control'])}}
+    </div>
+    <div class="form-group col-md-4">    
+        {{ Form::label('sector','Sector') }}
+        {{ Form::text ('sector',null,['class' =>'form-control'])}}
+    </div>
+</div>
+<div class="form-row">
+    <div class="form-group col-md-8">
+    {{ Form::label('cp','CP') }}
+    {{ Form::text ('cp',null,['class' =>'form-control'])}}
+    </div>
+
+    <div class="form-group col-md-4">    
+        {{ Form::label('cif','Cif') }}
+        {{ Form::text ('cif',null,['class' =>'form-control'])}}
+    </div>
+</div>
+<div class="form-row">
+    <div class="form-group col-md-6">
+        {{ Form::label('poblacion','Población') }}
+        {{ Form::text ('poblacion',null,['class' =>'form-control'])}}
+    </div>
+
+    <div class="form-group col-md-6">    
+        {{ Form::label('provincia','Provincia') }}
+        {{ Form::text ('provincia',null,['class' =>'form-control'])}}
+    </div>
+</div>
+<div class="form-row">
+    <div class="form-group col-md-4">
+        {{ Form::label('telefono','Teléfono') }}
+        {{ Form::text ('telefono',null,['class' =>'form-control'])}}
+    </div>
+
+    <div class="form-group col-md-8">    
+        {{ Form::label('mail','Email') }}
+        {{ Form::text ('mail',null,['class' =>'form-control'])}}
+    </div>
+</div>
+<div class="form-row">
+    <div class="form-group col-md-4">
+        {{ Form::label('cContable','C. Contable') }}
+        {{ Form::text ('c.Contable',null,['class' =>'form-control'])}}
+    </div>
+
+    <div class="form-group col-md-8">    
+        {{ Form::label('personaCont','Persona Contable') }}
+        {{ Form::text ('personaCont',null,['class' =>'form-control'])}}
+    </div>
 </div>
 <div class="form-group">
-    {{ Form::label('empresa_id','Empresa') }}
-    {{Form::select('empresa_id',$empresas, null, ['placeholder'=>'Seleccionar una Empresa'])}}
+    {{ Form::label('canal_id','Subcanal de Venda') }}
+    {{Form::select('canal_id',$empresas, null, ['placeholder'=>'Seleccionar Subcanal de Venda'])}}
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">    
+        {{ Form::label('observacions','Observacions') }}
+        {{Form::textarea('observacions', null, ['class' =>'form-control'])}}
+    </div>
+    <div class="form-group col-md-6">    
+        {{ Form::label('objectius','Objectius') }}
+        {{Form::textarea('objectius', null, ['class' =>'form-control'])}}
+    </div>
 </div>
 
 <div class="form-group">
-    {{ Form::submit('Guardar',['class'=>'btn btn-sm btn-primary']) }}
+    {{ Form::submit('Guardar',['class'=>'btn btn-primary']) }}
+    <a href="{{ route('clients.index')}}" class="btn btn-danger">Cancelar</a>
 </div>
 
 
